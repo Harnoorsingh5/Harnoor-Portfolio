38966983bc346c03d2e4a579b94ac34c88107b97
Harnoor Singh <harnoor.singh539@gmail.com>
2020-07-18 01:01:50 -0400

    Updates

---
+++ precache-manifest.2439964a4eb30cc209c15bb7cf93d9f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1fc7ef61bcb38c95a67",
+    "url": "/harnoor-portfolio/static/js/runtime~main.b1fc7ef6.js"
+  },
+  {
+    "revision": "44abc95acb3029a5df2e",
+    "url": "/harnoor-portfolio/static/js/main.44abc95a.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/harnoor-portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "44abc95acb3029a5df2e",
+    "url": "/harnoor-portfolio/static/css/main.eb318fe5.chunk.css"
+  },
+  {
+    "revision": "8103834502430e33b4d40356bf2fe3ec",
+    "url": "/harnoor-portfolio/index.html"
+  }
+];
